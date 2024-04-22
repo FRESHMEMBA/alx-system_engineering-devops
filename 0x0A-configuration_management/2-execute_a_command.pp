112037360e9cfdeac1902913bf041f5768892031
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-22 23:26:37 +0200

    2. Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
 # Executes a command
 
-exec {'killmenow':
-    command     => 'pkill -f killmenow',
-    path        => 'bin:/usr/bin',
-    refreshonly => true,
+exec {'pkill killmenow':
+    path => '/usr/bin:usr/sbin:/bin'
 }
