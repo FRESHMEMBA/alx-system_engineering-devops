ea7de766ff21eb0ad09ed14a8d4e25e5eead9520
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-07-01 08:06:56 +0000

    update 1-install _a_package to ensure python3 and pip3 exist before installing a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 # Installs a package
 
-package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip'
+# Ensure that python3 is installed
+package {'python3':
+    ensure => installed,
+}
+
+# Ensure that pip3 is installed
+pacakge {'python3-pip':
+    ensure => installed,
+}
+
+# Ensure Flask v2.1.0 is installed using pip3
+exec {'Install_flask':
+    command  => '/usr/bin/pip3 install flask==2.1.0',
+    unless   => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+    require  => package['python3', 'python3-pip'],
 }
