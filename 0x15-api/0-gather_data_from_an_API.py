24e530011936f7ac1be106e3ccef24971d864774
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-07-30 06:40:53 +0200

    get data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """
-For a given employee ID, information about his/her TODO list progress is returned.
+For a given employee ID, information about
+    his/her TODO list progress is returned.
 
 Uses the requests module to connect to the API
 The script accepts an integer as a parameter, which is the employee ID
-The script displays on the standard output the employee TODO list progress in this exact format:
-First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+The script displays on the standard output the employee TODO
+list progress in this exact format:
+First line: Employee EMPLOYEE_NAME is done with tasks(DONE/TOTAL):
 EMPLOYEE_NAME: name of the employee
-NUMBER_OF_DONE_TASKS: number of completed tasks
-TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks
-The second and N next lines display the title of completed tasks: TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+DONE: number of completed tasks
+TOTAL: total number of tasks, which is the sum of
+    completed and non-completed tasks
+The second and N next lines display the title of completed tasks:
+TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
 """
 
 from my_tools import get_employee_info
