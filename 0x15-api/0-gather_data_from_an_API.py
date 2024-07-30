a326a84e51cc3bf2d31465dfab344f8910cdd630
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-07-30 06:50:31 +0200

    get data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -23,9 +23,9 @@ if __name__ == "__main__":
     id = int(argv[1])
     employee_name, todos = get_employee_info(id=id)
     completed_tasks = [todo for todo in todos if todo.get("completed")]
-    complete = len(completed_tasks)
-    total = len(todos)
-    print(f"Employee {employee_name} is done with tasks({complete}/{total})")
+    number_of_complete_tasks = len(completed_tasks)
+    total_tasks = len(todos)
+    print(f"Employee {employee_name} is done with tasks({number_of_complete_tasks}/{total_tasks})")
 
     for task in completed_tasks:
         print(f"\t {task.get('title')}")
