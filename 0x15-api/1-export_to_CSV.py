ac78d4dea30233e750da38a94158a8a6c4576332
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-07-31 20:34:26 +0200

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,4 +16,8 @@ from my_tools import get_employee_info, export_to_csv
 if __name__ == "__main__":
     employee_id = int(argv[1])
     employee_name, todos = get_employee_info(employee_id)
-    export_to_csv(employee_id=employee_id, employee_name=employee_name, todos=todos)
+    export_to_csv(
+        employee_id=employee_id,
+        employee_name=employee_name,
+        todos=todos
+    )
