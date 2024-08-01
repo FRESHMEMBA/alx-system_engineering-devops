db7fb987d4001521bfb702a59243be9dfe76ea9f
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-08-01 02:11:20 +0200

    export to json

--- 0x15-api/my_tools.py
+++ 0x15-api/my_tools.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import csv
+import json
 import requests
 
 def get_employee_info(employee_id):
@@ -30,9 +31,31 @@ def export_to_csv(employee_id, employee_name, todos):
     Exports data to CSV format
     """
     filename = f"{employee_id}.csv"
-    with open(filename, 'w') as file:
-        writer = csv.writer(file)
+    with open(filename, 'w') as csv_file:
+        writer = csv.writer(csv_file)
         writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
         for todo in todos:
             writer.writerow([employee_id, employee_name, todo.get("completed"), todo.get("title")])
+
+
+def export_to_json(employee_id, employee_name, todos):
+    """
+    Exports data to json
+    """
+    filename = f"{employee_id}.json"
+    tasks = []
+    json_obj = {}
+    with open(filename, 'w') as file:
+        for todo in todos:
+            task_info = {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": employee_name
+            }
+            tasks.append(task_info)
+        
+        data = {str(employee_id): tasks}
+
+        with open(filename, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
