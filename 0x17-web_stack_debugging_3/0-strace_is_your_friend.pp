5c3656d007db6721bc23946264b27e56ec6a9c6c
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-08-14 15:12:12 +0200

    strace is my friend indeed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,2 +1,15 @@
-# Using strace, find out why Apache is returning a 500 error. Once you find the issue,
-# fix it and then automate it using Puppet (instead of using Bash).
+# Puppet Manifest: 0-strace_is_your_friend.pp
+
+# Ensure the correct file reference in wp-settings.php
+file_line { 'fix-wp-locale-typo':
+  path    => '/var/www/html/wp-settings.php',
+  match   => 'class-wp-locale.phpp',
+  line    => 'require_once(ABSPATH . WPINC . "/class-wp-locale.php");',
+}
+
+# Restart Apache to apply the changes
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => File_line['fix-wp-locale-typo'],
+}
