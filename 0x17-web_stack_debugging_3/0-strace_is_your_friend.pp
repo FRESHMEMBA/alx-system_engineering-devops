0f7974c74d41cec9ad8e4f6c83d29496dac3aa7c
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-08-14 15:23:16 +0200

    strace is my friend indeed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,14 +2,14 @@
 
 # Ensure the correct file reference in wp-settings.php
 file_line { 'fix-wp-locale-typo':
-  path    => '/var/www/html/wp-settings.php',
-  match   => 'class-wp-locale.phpp',
-  line    => 'require_once(ABSPATH . WPINC . "/class-wp-locale.php");',
+    path    => '/var/www/html/wp-settings.php',
+    match   => 'class-wp-locale.phpp',
+    line    => 'require_once(ABSPATH . WPINC . "/class-wp-locale.php);',
 }
 
 # Restart Apache to apply the changes
 service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => File_line['fix-wp-locale-typo'],
+    ensure  => running,
+    enable  => true,
+    require => File_line['fix-wp-locale-typo'],
 }
