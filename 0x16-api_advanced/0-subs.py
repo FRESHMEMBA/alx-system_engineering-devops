b028196d92ba032105883813af68c556c15edf66
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-08-15 18:08:57 +0200

    0 subs and more

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,8 +17,17 @@ Ensure that you are not following redirects.
 """
 
 
+import requests
+
+
 def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit
     """
-    return 0
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    else:
+        return 0
