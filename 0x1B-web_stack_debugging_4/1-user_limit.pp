672769fa405a05b6efe8bd3b4bc9044cec5fe87d
FRESHMEMBA <smazibuko023@student.wethinkcode.co.za>
2024-09-02 23:17:44 +0200

    remove tabs

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,7 +3,7 @@
 # Ensure the limits are set for the holberton user
 exec { 'change-os-configuration-for-holberton-user':
     command => 'echo "holberton soft nofile 65535" >> /etc/security/limits.conf ' +
-               '&& echo "holberton hard nofile 65535" >> /etc/security/limits.conf',
+        '&& echo "holberton hard nofile 65535" >> /etc/security/limits.conf',
     unless  => 'grep -q "holberton.*nofile" /etc/security/limits.conf',
 }
 
