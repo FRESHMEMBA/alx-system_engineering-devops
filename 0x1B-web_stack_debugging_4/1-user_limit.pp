bafa2770a24826b605b9c5b2739dcaa4f7298147
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-09-23 00:44:43 +0200

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,6 @@
 # This manifest increases the number of open file descriptors for the 'holberton' user
 
-# Ensure the limits are set for the holberton user
-exec { 'change-os-configuration-for-holberton-user':
-    command => 'echo "holberton soft nofile 65535" >> /etc/security/limits.conf ' +
-        '&& echo "holberton hard nofile 65535" >> /etc/security/limits.conf',
-    unless  => 'grep -q "holberton.*nofile" /etc/security/limits.conf',
-}
-
-# Restart the session or apply the changes to take effect
-exec { 'apply-limits':
-    command => '/bin/su -c "ulimit -n 65535" holberton',
-    require => Exec['change-os-configuration-for-holberton-user'],
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
