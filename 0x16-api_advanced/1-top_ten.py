ec1bc2626cd8aca2d2664d9fdde5a8c7b740e312
FRESHMEMBA <smazibuko023@student.wethinkcode.co.za>
2024-09-23 13:36:41 +0200

    update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,6 +12,7 @@ Requirements:
 
 
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
@@ -19,27 +20,15 @@ def top_ten(subreddit):
     Prints the title of the firts 10 hot posts for a given subreddit.
     If the subreddit is invalid, prints None.
     """
-    # Define the URL for the subreddit
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-
-    # Define a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'MyRedditBot/0.1.1'}
-
+    user = {'User-Agent': 'bot'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        # Send a get request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            # Extract the posts
-            posts =  response.json()['data']['children']
-
-            # Print the title of each post
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            # Print None if the subreddit is invalid
-            print(None)
-    
-    except requests.RequestException:
-        # Print None in case of any request exception
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
         print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
