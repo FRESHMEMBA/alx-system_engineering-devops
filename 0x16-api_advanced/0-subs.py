73644c86032d62cec227125cb3effd7d993fe772
FRESHMEMBA <smazibuko023@student.wethinkcode.co.za>
2024-09-23 03:27:26 +0200

    update 0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -25,24 +25,12 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit.
     If the subreddit is invalid, returns 0.
     """
-    # Define the URL for the subreddit
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    # Define a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'MyRedditBot/0.0.1'}
-
-    try:
-        # Send a get request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            # Extract and return the number of subscribers
-            return response.json()['data']['subscribers']
-        else:
-            # Return 0 if the subreddit is invalid
-            return 0
-
-    except requests.RequestException:
-        # Return 0 in case of any request exception
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
