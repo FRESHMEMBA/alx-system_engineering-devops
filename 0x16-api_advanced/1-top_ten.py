bcbcc031d9b544828b641fc44856f7bbc1881c46
FRESHMEMBA <smazibuko023@student.wethinkcode.co.za>
2024-09-27 12:59:52 +0200

    reecreate 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the title of the first 10 hot posts listed
+for a given subreddit.
+
+Requirements:
+    Prototype: def top_ten(subreddit)
+    If not a valid subreddit, print None
+    NOTE: Invalid subreddits may return a redirect to search results.
+        Ensure that you are not following redirects.
+"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the title of the firts 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints None.
+    """
+    user = {'User-Agent': 'bot'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
+
+# if __name__ == "__main__":
+#     top_ten(argv[1])
